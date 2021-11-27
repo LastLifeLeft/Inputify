LastLifeLeft/Inputify

commit 2ff9ba200cd9f0e7bda3a9424085549dd4ff6c9f

Cleanup of the ShortCut() procedure

Includes/PopupWindow.pbi
@@ -337,124 +337,51 @@
 	EndProcedure
 	
 	Procedure ShortCut(Control, Shift, Alt,Vkey)
-		;TODO: Clean this mess up!
-		If Vkey = #VK_SHIFT
-			If *LatestWindow And *LatestWindow\Status < #Dying 
-				If *LatestWindow\Vkey = #VK_CONTROL And *LatestWindow\Combo = 1
-					MainWindow::InputArray(#VK_SHIFT) = MainWindow::InputArray(#VK_CONTROL)
-					MainWindow::InputArray(#VK_CONTROL) = #False
-					AddKey(*LatestWindow\Window, Vkey)
-				ElseIf *LatestWindow\Vkey = #VK_CONTROL + Vkey
-					Create(*LatestWindow\Vkey)
-					MainWindow::InputArray(#VK_SHIFT) = *LatestWindow\Window
-				Else
-					AddWindowTimer(*LatestWindow\Window, #Timer_Duration, General::Preferences(General::#Pref_Duration))
-					MainWindow::InputArray(#VK_CONTROL) = #False
-					*LatestWindow = 0
-					MainWindow::InputArray(#VK_SHIFT) = Create(#VK_CONTROL)
-					AddKey(*LatestWindow\Window, #VK_SHIFT)
-				EndIf
-			Else
-				MainWindow::InputArray(#VK_CONTROL) = #False
-				*LatestWindow = 0
-				MainWindow::InputArray(#VK_SHIFT) = Create(#VK_CONTROL)
-				AddKey(*LatestWindow\Window, #VK_SHIFT)
-			EndIf
-		ElseIf Vkey = #VK_MENU
-			If *LatestWindow And *LatestWindow\Status < #Dying 
-				If *LatestWindow\Vkey = #VK_CONTROL And *LatestWindow\Combo = 1
-					MainWindow::InputArray(#VK_MENU) = MainWindow::InputArray(#VK_CONTROL)
-					MainWindow::InputArray(#VK_CONTROL) = #False
-					AddKey(*LatestWindow\Window, Vkey)
-				ElseIf *LatestWindow\Vkey = #VK_CONTROL + Vkey
-					Create(*LatestWindow\Vkey)
-					MainWindow::InputArray(#VK_MENU) = *LatestWindow\Window
-				Else
-					AddWindowTimer(*LatestWindow\Window, #Timer_Duration, General::Preferences(General::#Pref_Duration))
-					MainWindow::InputArray(#VK_CONTROL) = #False
-					*LatestWindow = 0
-					MainWindow::InputArray(#VK_MENU) = Create(#VK_CONTROL)
-					AddKey(*LatestWindow\Window, #VK_MENU)
-				EndIf
-			Else
-				MainWindow::InputArray(#VK_CONTROL) = #False
-				*LatestWindow = 0
-				MainWindow::InputArray(#VK_MENU) = Create(#VK_CONTROL)
-				AddKey(*LatestWindow\Window, #VK_MENU)
-			EndIf
-		Else
-			If *LatestWindow And *LatestWindow\Status < #Dying 
-				If *LatestWindow\Vkey = Control * #VK_CONTROL + Shift * #VK_SHIFT + Alt * #VK_MENU And *LatestWindow\Combo = 1
-					MainWindow::InputArray(Vkey) = *LatestWindow\Window
-					MainWindow::InputArray(#VK_CONTROL) = #False
-					MainWindow::InputArray(#VK_SHIFT) = #False
-					MainWindow::InputArray(#VK_MENU) = #False
-					
-					AddKey(*LatestWindow\Window, Vkey)
-					
-				ElseIf *LatestWindow\Vkey = Control * #VK_CONTROL + Shift * #VK_SHIFT + Alt * #VK_MENU + Vkey
-					Create(*LatestWindow\Vkey)
-					MainWindow::InputArray(Vkey) = *LatestWindow\Window
-				Else
-					AddWindowTimer(*LatestWindow\Window, #Timer_Duration, General::Preferences(General::#Pref_Duration))
-					*LatestWindow = 0
-					MainWindow::InputArray(#VK_CONTROL) = #False
-					MainWindow::InputArray(#VK_SHIFT) = #False
-					MainWindow::InputArray(#VK_MENU) = #False
-					
-					If Control
-						MainWindow::InputArray(Vkey) = Create(#VK_CONTROL)
-					EndIf
-					
-					If Shift
-						If MainWindow::InputArray(Vkey)
-							AddKey(MainWindow::InputArray(Vkey), #VK_SHIFT)
-						Else
-							MainWindow::InputArray(Vkey) = Create(#VK_SHIFT)
-						EndIf
-					EndIf
-					
-					If Alt
-						If MainWindow::InputArray(Vkey)
-							AddKey(MainWindow::InputArray(Vkey), #VK_MENU)
-						Else
-							MainWindow::InputArray(Vkey) = Create(#VK_MENU)
-						EndIf
-					EndIf
-					
-					AddKey(MainWindow::InputArray(Vkey), Vkey)
-					
-				EndIf
-			Else
-				AddWindowTimer(*LatestWindow\Window, #Timer_Duration, General::Preferences(General::#Pref_Duration))
-				*LatestWindow = 0
+		If *LatestWindow And *LatestWindow\Status < #Dying 
+			If *LatestWindow\Vkey = Control * #VK_CONTROL + (Shift - Bool(Vkey = #VK_SHIFT)) * #VK_SHIFT + (Alt - Bool(Vkey = #VK_MENU)) * #VK_MENU And *LatestWindow\Combo = 1
+				MainWindow::InputArray(Vkey) = *LatestWindow\Window
 				MainWindow::InputArray(#VK_CONTROL) = #False
 				MainWindow::InputArray(#VK_SHIFT) = #False
 				MainWindow::InputArray(#VK_MENU) = #False
 				
-				If Control
-					MainWindow::InputArray(Vkey) = Create(#VK_CONTROL)
-				EndIf
+				AddKey(*LatestWindow\Window, Vkey)
 				
-				If Shift
-					If MainWindow::InputArray(Vkey)
-						AddKey(MainWindow::InputArray(Vkey), #VK_SHIFT)
-					Else
-						MainWindow::InputArray(Vkey) = Create(#VK_SHIFT)
-					EndIf
-				EndIf
+				ProcedureReturn #False
+			ElseIf *LatestWindow\Vkey = Control * #VK_CONTROL + Shift * #VK_SHIFT + Alt * #VK_MENU + Vkey
+				Create(*LatestWindow\Vkey)
+				MainWindow::InputArray(Vkey) = *LatestWindow\Window
 				
-				If Alt
-					If MainWindow::InputArray(Vkey)
-						AddKey(MainWindow::InputArray(Vkey), #VK_MENU)
-					Else
-						MainWindow::InputArray(Vkey) = Create(#VK_MENU)
-					EndIf
-				EndIf
-				
-				AddKey(MainWindow::InputArray(Vkey), Vkey)
+				ProcedureReturn #False
 			EndIf
 		EndIf
+		
+		AddWindowTimer(*LatestWindow\Window, #Timer_Duration, General::Preferences(General::#Pref_Duration))
+		*LatestWindow = 0
+		MainWindow::InputArray(#VK_CONTROL) = #False
+		MainWindow::InputArray(#VK_SHIFT) = #False
+		MainWindow::InputArray(#VK_MENU) = #False
+		
+		If Control
+			MainWindow::InputArray(Vkey) = Create(#VK_CONTROL)
+		EndIf
+		
+		If Shift
+			If MainWindow::InputArray(Vkey)
+				AddKey(MainWindow::InputArray(Vkey), #VK_SHIFT)
+			Else
+				MainWindow::InputArray(Vkey) = Create(#VK_SHIFT)
+			EndIf
+		EndIf
+		
+		If Alt
+			If MainWindow::InputArray(Vkey)
+				AddKey(MainWindow::InputArray(Vkey), #VK_MENU)
+			Else
+				MainWindow::InputArray(Vkey) = Create(#VK_MENU)
+			EndIf
+		EndIf
+		
+		AddKey(MainWindow::InputArray(Vkey), Vkey)
 	EndProcedure
 	
 	Procedure SetScale(NewScale)
@@ -756,7 +683,7 @@
 	;}
 EndModule
 ; IDE Options = PureBasic 6.00 Beta 1 (Windows - x64)
-; CursorPosition = 61
-; FirstLine = 3
-; Folding = BkQA-
+; CursorPosition = 383
+; FirstLine = 48
+; Folding = DkQA-
 ; EnableXP

Inputify.pbp
@@ -7,7 +7,7 @@
   <section name="data">
     <explorer view="C:\ProgramData\PureBasic\Examples\" pattern="0"/>
     <log show="1"/>
-    <lastopen date="2021-11-27 03:12" user="Poshu" host="DESKTOP-RGG8DMG"/>
+    <lastopen date="2021-11-27 03:29" user="Poshu" host="DESKTOP-RGG8DMG"/>
   </section>
   <section name="files">
     <file name="Includes\General.pbi">
@@ -20,7 +20,7 @@
     </file>
     <file name="Includes\PopupWindow.pbi">
       <config load="0" scan="1" panel="1" warn="1" lastopen="1" panelstate="++"/>
-      <fingerprint md5="d004520c9f0c3d658e8faa13d93da14a"/>
+      <fingerprint md5="5ed78844623172b1e1af07f7deb0f811"/>
     </file>
     <file name="Libraries\FlatMenu\FlatMenu.pbi">
       <config load="0" scan="1" panel="1" warn="1" lastopen="0" panelstate="+++"/>

commit d95d107a60c32185eea3838246dae1dea813ce8b

Bug fix : repeated Alt and Shift in shortcuts
Regression introduced in https://github.com/LastLifeLeft/Inputify/commit/2ff9ba200cd9f0e7bda3a9424085549dd4ff6c9f

Includes/PopupWindow.pbi
@@ -338,11 +338,11 @@
 	
 	Procedure ShortCut(Control, Shift, Alt,Vkey)
 		If *LatestWindow And *LatestWindow\Status < #Dying 
-			If *LatestWindow\Vkey = Control * #VK_CONTROL + (Shift - Bool(Vkey = #VK_SHIFT)) * #VK_SHIFT + (Alt - Bool(Vkey = #VK_MENU)) * #VK_MENU And *LatestWindow\Combo = 1
-				MainWindow::InputArray(Vkey) = *LatestWindow\Window
+			If *LatestWindow\Vkey = Control * #VK_CONTROL + (Shift * Bool(Not Vkey = #VK_SHIFT)) * #VK_SHIFT + (Alt * Bool(Not Vkey = #VK_MENU)) * #VK_MENU And *LatestWindow\Combo = 1
 				MainWindow::InputArray(#VK_CONTROL) = #False
 				MainWindow::InputArray(#VK_SHIFT) = #False
 				MainWindow::InputArray(#VK_MENU) = #False
+				MainWindow::InputArray(Vkey) = *LatestWindow\Window
 				
 				AddKey(*LatestWindow\Window, Vkey)
 				
@@ -683,7 +683,7 @@
 	;}
 EndModule
 ; IDE Options = PureBasic 6.00 Beta 1 (Windows - x64)
-; CursorPosition = 383
-; FirstLine = 48
-; Folding = DkQA-
+; CursorPosition = 343
+; FirstLine = 51
+; Folding = BkEA-
 ; EnableXP

Inputify.pbp
@@ -7,7 +7,7 @@
   <section name="data">
     <explorer view="C:\ProgramData\PureBasic\Examples\" pattern="0"/>
     <log show="1"/>
-    <lastopen date="2021-11-27 05:08" user="Poshu" host="DESKTOP-RGG8DMG"/>
+    <lastopen date="2021-11-27 05:22" user="Poshu" host="DESKTOP-RGG8DMG"/>
   </section>
   <section name="files">
     <file name="Includes\General.pbi">
@@ -20,7 +20,7 @@
     </file>
     <file name="Includes\PopupWindow.pbi">
       <config load="0" scan="1" panel="1" warn="1" lastopen="1" panelstate="++"/>
-      <fingerprint md5="5ed78844623172b1e1af07f7deb0f811"/>
+      <fingerprint md5="208e3948a6f98cedc1b4b9ff82fd0b23"/>
     </file>
     <file name="Libraries\FlatMenu\FlatMenu.pbi">
       <config load="0" scan="1" panel="1" warn="1" lastopen="0" panelstate="+++"/>